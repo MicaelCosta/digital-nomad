c4f61b4fb1588cdfc0af549e387de430d3039675
Micael Amorim <micael@autoware.com.br>
2025-10-01 08:45:29 -0300

    feat: implemented animation icon accordion

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Pressable, StyleSheet, View } from "react-native";
 import Animated, {
+  interpolate,
   SharedValue,
   useAnimatedStyle,
   useSharedValue,
@@ -18,15 +19,17 @@ type AccordionProps = {
 
 export function Accordion({ title, description }: AccordionProps) {
   const isOpen = useSharedValue(false);
+  const progress = useSharedValue(0); // 0 => 1
 
   function handleOpenPress() {
     isOpen.value = !isOpen.value;
+    progress.value = withTiming(isOpen.value ? 0 : 1, { duration: 500 });
   }
 
   return (
     <Pressable onPress={handleOpenPress}>
       <View>
-        <AccordionHeader title={title} />
+        <AccordionHeader title={title} progress={progress} />
         <AccordionBody description={description} isOpen={isOpen} />
       </View>
     </Pressable>
@@ -35,15 +38,26 @@ export function Accordion({ title, description }: AccordionProps) {
 
 type AccordionHeaderProps = {
   title: string;
+  progress: SharedValue<number>;
 };
 
-export function AccordionHeader({ title }: AccordionHeaderProps) {
+export function AccordionHeader({ title, progress }: AccordionHeaderProps) {
+  const iconAnimatedStyle = useAnimatedStyle(() => ({
+    transform: [
+      {
+        rotate: interpolate(progress.value, [0, 1], [0, -180]) + "deg",
+      },
+    ],
+  }));
+
   return (
     <View style={styles.header}>
       <Box flexShrink={1}>
         <Text variant="text16">{title}</Text>
       </Box>
-      <Icon name="Chevron-down" color="gray2" />
+      <Animated.View style={iconAnimatedStyle}>
+        <Icon name="Chevron-down" color="gray2" />
+      </Animated.View>
     </View>
   );
 }
@@ -61,16 +75,14 @@ export function AccordionBody({ description, isOpen }: AccordionBodyProps) {
     })
   ); */
 
-  const animatedStyle = useAnimatedStyle(() => {
-    return {
-      height: withTiming(height.value * Number(isOpen.value), {
-        duration: 500,
-      }),
-      /* height: isOpen.value
+  const animatedStyle = useAnimatedStyle(() => ({
+    height: withTiming(height.value * Number(isOpen.value), {
+      duration: 500,
+    }),
+    /* height: isOpen.value
         ? withTiming(height.value, { duration: 500 })
         : withTiming(0, { duration: 500 }), */
-    };
-  });
+  }));
 
   return (
     <Animated.View
